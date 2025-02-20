47df0c5be399ed3f99f8f7d448b04d54d17f181c
Mohammad Rahimi <m.rahimi.vasteras@gmail.com>
2025-02-20 11:12:58 +0100

    deletd old files

--- testing api/testcase_django.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from django.urls import reverse
-
-class ChatbotAPITest(TestCase):
-    def test_chatbot_endpoint(self):
-        url = reverse('chatbot')  # Use the name from `urlpatterns`
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content.decode(), "Chatbot Webside")
