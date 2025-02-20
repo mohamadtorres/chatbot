180ac9c193cef9aee4184c5f9587576273eb9654
Mohammad Rahimi <m.rahimi.vasteras@gmail.com>
2025-02-20 13:54:05 +0100

    base.html

--- chatbot_project/chatbot/views.py
+++ chatbot_project/chatbot/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse #Helps to return data in json format
 from django.template import loader
 
-def chatbot(request):
-    template = loader.get_template('base.html') #loader the template u want
-    return HttpResponse(template.render()) #render the template u loaded
+# def chatbot(request):
+#     template = loader.get_template('base.html') #loader the template u want
+#     return HttpResponse(template.render()) #render the template u loaded
+
