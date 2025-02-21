1b1d2549cff687514fb947ce94570be0fbba17ea
Mohammad Rahimi <m.rahimi.vasteras@gmail.com>
2025-02-21 11:15:49 +0100

    django api

--- chatbot_project/chatbot/urls.py
+++ chatbot_project/chatbot/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns= [
-    path('chatbot/', views.chatbot, name ='chatbot')
+    path('', views.chatbot, name ='chatbot')
 ]
--- chatbot_project/chatbot/views.py
+++ chatbot_project/chatbot/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+import requests
 from django.http import HttpResponse, JsonResponse #Helps to return data in json format
-from django.template import loader
+import json
+from django.views.decorators.csrf import csrf_exempt
+
+
+# from django.template import loader
 
 # def chatbot(request):
 #     template = loader.get_template('base.html') #loader the template u want
 #     return HttpResponse(template.render()) #render the template u loaded
 
+@csrf_exempt
+def chatbot_view(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        user_message = data.get('message')
