a62bd045b9dd0badc1cb7467ac6ca517a08bd231
Mohammad Rahimi <m.rahimi.vasteras@gmail.com>
2025-03-14 20:25:42 +0100

    issues with mathjax

--- chatbot_project/chatbot_project/settings.py
+++ chatbot_project/chatbot_project/settings.py
@@ -116,11 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    BASE_DIR / "chatbot/static",
 ]
-
+ # ✅ Make sure this is correct
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
